837033be7b0c4b5890271d17891cf8d015185f4c
hatt <hattrickhale@gmail.com>
2026-08-07 08:31:02 +0100

    test for capital letters

--- tests/test_anagram.py
+++ tests/test_anagram.py
@@ -4,6 +4,8 @@ from anagram import is_anagram
 class TestAnagram(unittest.TestCase):
     def test_contain_same_letters(self):
         self.assertTrue(is_anagram("dusty","study"))
+    def test_contain_capital_letters(self):
+        self.assertTrue(is_anagram("Dusty","Study"))
 
 if __name__ == "__main__":
     unittest.main()
