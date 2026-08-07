4f21b3f50ead207b5345a9a3c437f71df67c5813
hatt <hattrickhale@gmail.com>
2026-08-07 08:32:59 +0100

    added fix for capitalisation in text

--- anagram.py
+++ anagram.py
@@ -1,4 +1,4 @@
 def is_anagram(first_text: str, second_text: str) -> bool:
-    first_text = sorted(first_text)
-    second_text = sorted(second_text)
+    first_text = sorted(first_text.casefold())
+    second_text = sorted(second_text.casefold())
     return first_text == second_text
